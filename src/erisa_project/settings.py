57a86721a1687e090ebdc817b8697b38c55e40c1
zz-xx <31221615+zz-xx@users.noreply.github.com>
2025-09-10 15:38:37 -0400

    Add static root to settings.py for PythonAnywhere configuration.

--- src/erisa_project/settings.py
+++ src/erisa_project/settings.py
@@ -176,3 +176,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
+
+# For PythonAnywhere configuration
+# This is where collectstatic will dump all files for production
+STATIC_ROOT = BASE_DIR / "staticfiles"
